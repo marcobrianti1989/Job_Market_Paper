dfde02026300e09b6314e1a06a59a7211d2c109d
Marco Brianti <marcobrianti@live.it>
2018-08-20 17:58:54 -0400

    Good progress!

---
+++ Code/f_test.m
@@ -0,0 +1,21 @@
+function pvalue = f_test(res_r,res_ur,q,n,k)
+% Inputs
+% res_r  = residuals from restricted regression
+% res_ur = residuals from unrestricted regression
+% q      = no. of restrictions
+% k      = no. of regressors
+% n      = length of dataset (e.g time dimension)
+% Outputs
+% sig_level = the significance at which the null would be rejected
+
+SSR_R  = sum(res_r.^2);
+SSR_UR = sum(res_ur.^2);
+
+numerator   = (SSR_UR - SSR_R)/q;
+denominator = SSR_UR / (n-k-1);
+
+F = numerator / denominator;
+sig_level = fcdf(F, q, n-k-1);
+pvalue = 1 - sig_level;
+
+end
--- Code/main_file_Choleski.m
+++ Code/main_file_Choleski.m
@@ -12,7 +12,7 @@ close all
 % Reading Data
 filename                    = 'Quarterly';
 sheet                       = 'Quarterly Data';
-range                       = 'B1:AJ274';
+range                       = 'B1:AK274';
 do_truncation               = 1; %Do not truncate data. You will have many NaN
 [dataset, var_names]        = read_data(filename, sheet, range, do_truncation);
 dataset                     = real(dataset(1:end-1,:));
@@ -46,25 +46,74 @@ pc2                         = pc(:,2);
 pc3                         = pc(:,3);
 pc4                         = pc(:,4);
 
-% Define the system
-system_names  = {'GDP','Consumption','Investment','Hours','TFPUtil','FFR','SP5001','VXO'};
-%system_names  = {'VXO','GDP'};
-for i = 1:length(system_names)
-      system(:,i) = eval(system_names{i});
+% Define the system1
+system_names1  = {'VXO','GDP','Consumption','Investment','Hours','TFPUtil','FFR','GovSpending','pc1','pc2','pc3','pc4'};
+
+for i = 1:length(system_names1)
+      system1(:,i) = eval(system_names1{i});
 end
-TFPposition = find(strcmp('TFP', system_names));
-VXOposition = find(strcmp('VXO', system_names));
+TFPposition1 = find(strcmp('TFP', system_names1));
+VXOposition1 = find(strcmp('VXO', system_names1));
 
 % Choose the correct number of lags
 max_lags     = 4;
-[AIC,BIC,HQ] = aic_bic_hq(system,max_lags);
+[AIC,BIC,HQ] = aic_bic_hq(system1,max_lags);
 
 % Cholesky decomposition
-nlags                    = 3;
-[A,B,res,sigma]          = sr_var(system, nlags);
-asd
+nlags                        = 3;
+[A1,B1,res1,sigma1]          = sr_var(system1, nlags);
+
 % Check if the VAR is stationary
-test_stationarity(B');
+test_stationarity(B1');
+
+% Get Structural Shocks
+ss1 = (inv(A1)*res1')';
+
+% regreg = (res1(:,2)'*res1(:,1))/(res1(:,1)'*res1(:,1));
+% ss1_con = res1(:,2)/A1(2,2) - res1(:,1)*regreg/A1(2,2);
+% q = 7;
+% k = q;
+% n = length(ss1(:,1));
+% pvalue = f_test(ss1_con,ss1(:,1), q, n, k);
+
+% Define the system2
+system_names2  = {'GDP','Consumption','Investment','Hours','TFPUtil','FFR','GovSpending','VXO','pc1','pc2','pc3','pc4'};
+%system_names2  = {'GDP','VXO'};
+
+for i = 1:length(system_names2)
+      system2(:,i) = eval(system_names2{i});
+end
+TFPposition2 = find(strcmp('TFP', system_names2));
+VXOposition2 = find(strcmp('VXO', system_names2));
+
+% Cholesky decomposition
+[A2,B2,res2,sigma2] = sr_var(system2, nlags);
+
+% Get Structural Shocks
+ss2 = (inv(A2)*res2')';
+
+y = res2(:,end);
+x = res2(:,1:end-1);
+
+b = (x'*y)'*(x'*x)^(-1);
+
+sh = y - x*b';
+
+corr(ss2(:,end-4),res1(:,1))
+
+% Correlation between the two identifications
+corr(res1(:,1),ss2(:,end))
+plot(Time(4:end),res1(:,1)/A1(1,1),'linewidth',2)
+hold on
+plot(Time(4:end),ss2(:,end-4),'linewidth',2)
+legend('VXO first','VXO last')
+
+q = 7;
+k = q;
+n = length(ss1(:,1));
+pvalue = f_test(y,sh,q,n,k);
+
+asdf
 
 % Create dataset from bootstrap
 nburn             = 0;
@@ -95,9 +144,6 @@ which_shocks      = [1 2 3];
 shocknames        = {'Technology Shock','Monetary Policy Shock','Uncertainty Shock'};
 plot_single_IRFs_2CIs(IRFs,ub1,lb1,ub2,lb2,H,which_shocks,shocknames,...
       system_names, which_ID, print_figs, use_current_time,base_path)
-
-% Get Structural Shocks
-ss = (inv(A)*res')';
 
 % Create Figure for Structural Shocks
 figure
